3a4bd65a9134a87fc9defafa6c672787d952f71c
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-18 21:36:47 +0100

    Sign Out

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import  supabase  from '../../supbaseClient';
+import {useNavigate} from 'react-router-dom';
 
 const Dashboard =()=> {
+    const navigate = useNavigate();
+  const signOut = async ()=>{
+       const {error}= await supabase.auth.signOut();
+       if(error) throw error;
+       navigate("/login");
+  }
   return (
-    <div>Dashboard</div>
+    <div>
+    <h1>Hello , you are logged in.</h1>
+    <button onClick={signOut}>Sign out</button>
+    </div>
   )
 }
 
