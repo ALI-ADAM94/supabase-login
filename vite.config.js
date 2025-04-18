f05d34889137cd206423914fbe9c75c0e25944aa
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-18 21:09:35 +0100

    One

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_LOGIN_PATH || "/Login-and-Signup",
+  base: process.env.VITE_LOGIN_PATH || "/supabase-login",
 })
