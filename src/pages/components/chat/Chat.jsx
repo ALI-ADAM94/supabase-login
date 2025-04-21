507860adca90b6e471f8b9f887acf4f1a00fc4a4
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-21 23:30:27 +0100

    as

--- src/pages/components/chat/Chat.jsx
+++ src/pages/components/chat/Chat.jsx
@@ -27,14 +27,14 @@ const Chat = () => {
         </div>
       </div>
       <div className='icons'>
-        <img src='/src/assets/phone.png' alt=''/>
-        <img src='/src/assets/video.png' alt=''/>
-        <img src='/src/assets/info.png' alt=''/>
+        <img src='/phone.png' alt=''/>
+        <img src='/video.png' alt=''/>
+        <img src='/info.png' alt=''/>
       </div>
     </div>
     <div className='center'>
       <div className='message '>
-        <img src='/src/assets/avatar.png' alt=''/>
+        <img src='/avatar.png' alt=''/>
         <div className='texts'>
           <p>First message in my App</p>
           <span>1 min ago</span>
@@ -48,7 +48,7 @@ const Chat = () => {
         </div>
       </div>
       <div className='message '>
-      <img src='./avatar.png' alt=''/>
+      <img src='/avatar.png' alt=''/>
         <div className='texts'>
           <p>First message in my App</p>
           <span>1 min ago</span>
@@ -61,7 +61,7 @@ const Chat = () => {
         </div>
       </div>
       <div className='message '>
-      <img src='/src/assets/avatar.png' alt=''/>
+      <img src='/avatar.png' alt=''/>
         <div className='texts'>
           <p>First message in my App</p>
           <span>1 min ago</span>
@@ -69,7 +69,7 @@ const Chat = () => {
       </div>
       <div className='message own'>
         <div className='texts'>
-        <img src='/src/assets/info.png' alt=''/>
+        <img src='/info.png' alt=''/>
           <p>First message in my App</p>
           <span>1 min ago</span>
         </div>
@@ -78,13 +78,13 @@ const Chat = () => {
     </div>
     <div className='bottom'>
       <div className='icons'>
-        <img src='/src/assets/img.png' alt=''/>
-        <img src='/src/assets/camera.png' alt=''/>
-        <img src='/src/assets/mic.png' alt=''/>
+        <img src='/img.png' alt=''/>
+        <img src='/camera.png' alt=''/>
+        <img src='/mic.png' alt=''/>
       </div>
       <input type='text' placeholder='Type a message...' value={text} onChange={(e)=>setText(e.target.value)}/>
       <div className='emoji'>
-        <img src='/src/assets/emoji.png' alt='' onClick={()=>setOpen((prev) => !prev)}/>
+        <img src='/emoji.png' alt='' onClick={()=>setOpen((prev) => !prev)}/>
          <div className='picker'>
           <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
          </div>
