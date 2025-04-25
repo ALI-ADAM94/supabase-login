1528c59f6215e7f12154d1d2960c192798c783cf
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-25 22:23:06 +0100

    e

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
               <br/>
         <h2 className="text-2xl font-bold text-center text-white mb-6"></h2>
         
-        {message && <div className="mb-4 text-red-600 text-sm">{message}</div>}
+        {message && <div className="mb-4 text-red-600 text-sm">{message}<br/></div>}
 
         <form onSubmit={handleSubmit} className=" space-y-6 justify-center">
           <input
@@ -58,7 +58,7 @@ const Login = () => {
             required
             className="w-full mb-4 px-4 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-        
+        <br/> <br/>
           <input
             type="password"
             placeholder="Password"
@@ -67,7 +67,7 @@ const Login = () => {
             required
             className="w-full mb-4 px-4 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-  
+        <br/> <br/>
           <motion.button
             type="submit"
             className="w-full mb-4 bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700 transition-colors"
