8866aed028b143c0870d64146c4ba72b58a31737
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-25 20:01:35 +0100

    lll

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import  {Link}  from 'react-router-dom'
+
 import { useNavigate } from 'react-router-dom';
 import Login from './Login';
 const Home =()=> {
-  const navigate = useNavigate();
-        navigate(<Login/>)
+  //const navigate = useNavigate();
+    //    navigate(<Login/>)
   return (
     <div>
      
