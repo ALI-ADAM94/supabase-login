a191a483ab4b456d0094aa2163f30c818979d249
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-25 20:59:02 +0100

    home.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Login from './Login';
-
+import './Home.css';
 const Home =()=> {
- 
  
   return (
 <div className="flex items-center justify-center min-h-screen p-[5px]">
