3bb9b14c87026b4a7ed7b00a5f42a5f98ae5737a
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-25 20:06:01 +0100

    f

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,9 @@ const Home =()=> {
   //const navigate = useNavigate();
     //    navigate(<Login/>)
   return (
-         <div className="min-h-screen flex items-center justify-center bg-gray-100">
-         <div className="w-full max-w-md p-6 bg-white rounded-2xl shadow-lg">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="w-full max-w-md p-6 bg-white border border-gray-300 rounded-2xl shadow-lg">
+    
         <Login/>
 
     </div>
