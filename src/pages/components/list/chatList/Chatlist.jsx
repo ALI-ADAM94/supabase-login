641e0eaf8baaca624c62eaf6aa24e9809c201a97
ALI-ADAM94 <aliadam081194@gmail.com>
2025-05-08 21:07:27 +0100

    q

--- src/pages/components/list/chatList/Chatlist.jsx
+++ src/pages/components/list/chatList/Chatlist.jsx
@@ -55,8 +55,7 @@ const Chatlist = () => {
              <img src='./avatar.png' alt='' />
              <div className='texts'>
                <span>{user.username} 
-                
-                <small class="top-0 right-0 mr-0 bg-blue-500 text-white rounded-full w-8 h-8 text-center flex items-center justify-center">{user.message_count}</small>
+               <small class="inline-block bg-blue-500 text-white rounded-full w-8 h-8 text-center  items-center justify-center float-right">{user.message_count}</small>
                </span>
            
               
